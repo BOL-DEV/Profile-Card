934d28283911966b7fc111ee7de0880a146c62fb
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2024-07-11 15:13:54 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <div className="sub-container">
+        <Image />
+        <Text />
+      </div>
+    </div>
+  );
+}
+
+const Image = () => {
+  return (
+    <div className="image-container">
+      <img src="./IMG_E1908.JPG" alt="habeeb" />
+    </div>
+  );
+};
+
+const Text = () => {
+  return (
+    <div className="text">
+      <h1> Abdulakeem habeeb </h1>
+      <p>
+        {" "}
+        Am a frontend web developer. When not coding, I like playing football or
+        cook(and eat), or to just enjoy the go on a ride to see the view of the
+        city{" "}
+      </p>
+      <div className="sticker-container">
+        <Sticker content="HTML5 + CSS3 💪" color="red" />
+        <Sticker content="JAVASCRIPT 💪" color="yellow" />
+        <Sticker content="WEB DESIGN 👍" color="green" />
+        <Sticker content="GIT AND GITHUB 👍" color="pink" />
+        <Sticker content="REACT 💪" color="blue" />
+        <Sticker content="TYPESCRIPT 😃" color="aqua" />
+      </div>
+    </div>
+  );
+};
+
+const Sticker = (props) => {
+  return (
+    <div className="sticker" style={{ backgroundColor: props.color }}>
+      <p>{props.content} </p>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
